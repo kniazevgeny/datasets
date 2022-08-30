43e53be43f70c34b1df75e456c251a2fae0ae064
kniazevgeny <eugene.kniazev@gmail.com>
2022-08-30 13:15:28 +0300

    fix: transition after F5

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,13 @@ export default class App extends Vue {
     if (toPath[toPath.length - 1] === '/') toPath = toPath.slice(0, -1)
     if (fromPath[fromPath.length - 1] === '/') fromPath = fromPath.slice(0, -1)
 
+    // Process reload (F5)
+    if (fromPath === '') {
+      console.log('empty')
+      this.transitionName = 'fade'
+      return 1
+    }
+
     let depthTo: Number = routes.indexOf(toPath)
     let depthFrom: Number = routes.indexOf(fromPath)
 
@@ -45,7 +52,7 @@ export default class App extends Vue {
     if (depthFrom < 0) depthFrom = 99
     if (depthTo < 0) depthTo = 99
 
-    console.log(depthFrom, depthTo)
+    console.log(typeof fromPath === 'string' && fromPath === '', toPath, depthFrom, depthTo)
     this.transitionName = depthTo < depthFrom ? 'slide-right' : 'slide-left'
   }
 }
@@ -71,18 +78,22 @@ export default class App extends Vue {
   transition: opacity 0.175s ease-out;
 }
 
-.slide-left-enter, .slide-right-leave-active, .slide-left-enter-active {
+.slide-left-enter,
+.slide-right-leave-active,
+.slide-left-enter-active {
   opacity: 0;
   -webkit-transform: translate(100px, 0);
   transform: translate(100px, 0);
 }
-.slide-left-leave-active, .slide-right-enter, .slide-right-enter-active{
+.slide-left-leave-active,
+.slide-right-enter,
+.slide-right-enter-active {
   opacity: 0;
   -webkit-transform: translate(-100px, 0);
   transform: translate(-100px, 0);
 }
 .child-view {
   position: absolute;
-  transition: all .175s cubic-bezier(0.44, 0.05, 0.3, 1);
+  transition: all 0.175s cubic-bezier(0.44, 0.05, 0.3, 1);
 }
 </style>
