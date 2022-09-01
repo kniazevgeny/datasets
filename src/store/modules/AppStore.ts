c92c288819cae8538b86241790fbcb056a483aad
kniazevgeny <eugene.kniazev@gmail.com>
2022-09-01 23:12:57 +0300

    upgrade yarn-actions

--- src/store/modules/AppStore.ts
+++ src/store/modules/AppStore.ts
@@ -1,4 +1,4 @@
-import { User } from '@/models/User'
+import { User } from '../../models/User'
 import { VuexModule, Module, Mutation } from 'vuex-module-decorators'
 
 @Module({ namespaced: true, name: 'AppStore' })
