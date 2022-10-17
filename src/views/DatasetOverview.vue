503e01e66a2a2e72ac66a534d9186e80cf17e8f8
kniazevgeny <eugene.kniazev@gmail.com>
2022-10-17 19:16:11 +0300

    feat: tooltips for data-table headers

--- src/views/DatasetOverview.vue
+++ src/views/DatasetOverview.vue
@@ -78,6 +78,71 @@ v-layout(style='width: 100%')
         multi-sort,
         disable-pagination
       )
+        template(v-slot:header.protein='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.mutation='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.ddG='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.sequence='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.pdb='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.chain='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.uniprot='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.alphafolddb='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.T='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.pH='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.method='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.measure='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
+        template(v-slot:header.doi='{ header }')
+          v-tooltip(top max-width='475')
+            template(v-slot:activator="{ on, attrs }")
+              span(v-on='on', v-bind='attrs') {{ header.text }}
+            span {{ header.description }}  
         template(v-slot:item.pdb='{ item }')
           a(
             :href='`https://www.rcsb.org/structure/${item.pdb}`',
@@ -278,27 +343,29 @@ export default class Datasets extends Vue {
       {
         text: 'protein name',
         value: 'protein',
+        description: 'Name of the protein studied in the experiment.',
         sortable: true,
         align: 'start',
       },
       {
         text: 'Mutation',
         value: 'mutation',
+        description: 'Mutation studied in the experiment. Residue numbering corresponds to that in the PDB structure. If no PDB structure is available, the residue is numbered according to the sequence specifieed in "Sequence" column.',
         align: 'start',
         sortable: true,
       },
-      { text: 'ΔΔG', value: 'ddG', sortable: true },
-      { text: 'Sequence', value: 'sequence', sortable: true },
-      { text: 'PDB ID', value: 'pdb', sortable: true, align: 'start' },
-      { text: 'chain', value: 'chain', sortable: false, align: 'start' },
-      { text: 'uniprot', value: 'uniprot', sortable: true },
-      { text: 'AlphaFold DB', value: 'alphafolddb', sortable: true },
+      { text: 'ΔΔG', value: 'ddG', description: 'Free energy change of folding, kcal/mol. Negative values denote stabilization.', sortable: true },
+      { text: 'Sequence', value: 'sequence', description: 'The sequence of the protein used in the experiment.', sortable: true },
+      { text: 'PDB ID', value: 'pdb', description: 'PDB ID of the protein structure if available.', sortable: true, align: 'start' },
+      { text: 'chain', value: 'chain', description: 'Chain identifier of the protein structure.', sortable: false, align: 'start' },
+      { text: 'uniprot', value: 'uniprot', description: 'Chain identifier of the protein structure.', sortable: true },
+      { text: 'AlphaFold DB', value: 'alphafolddb', description: 'Protein model in AlphaFold database.', sortable: true },
       // { text: 'organism', value: 'organism', sortable: true, align: 'start' },
-      { text: 'Temperature', value: 'T', sortable: true },
-      { text: 'pH', value: 'pH', sortable: true },
-      { text: 'method', value: 'method', sortable: true },
-      { text: 'measure', value: 'measure', sortable: true },
-      { text: 'reference', value: 'doi', sortable: true },
+      { text: 'Temperature', value: 'T', description: 'Temperature of the experiment in kelvins.', sortable: true },
+      { text: 'pH', value: 'pH', description: 'pH of the experiment.', sortable: true },
+      { text: 'method', value: 'method', description: 'Method of measuring the folding free energy change in the experiment.', sortable: true },
+      { text: 'measure', value: 'measure', description: 'Signal measured in the experiment.', sortable: true },
+      { text: 'reference', value: 'doi', description: 'Experiment reference.', sortable: true },
     ],
   }
 
