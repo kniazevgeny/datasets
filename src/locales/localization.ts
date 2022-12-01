3bd249179187ebb71dfb95a898ff41fb573a19eb
kniazevgeny <eugene.kniazev@gmail.com>
2022-12-01 20:20:01 +0300

    feat: lab page massive update

--- src/locales/localization.ts
+++ src/locales/localization.ts
@@ -4,10 +4,131 @@ export const messages = {
     notFound: 'Страница не найдена',
   },
   en: {
-    title: 'title',
+    title: 'Datasets project',
     notFound: 'Page not found',
     cookieMessage: 'We are using cookies',
     cookieButton: 'Fine👌',
+    laboratoryName: 'Ivankov Lab',
+    laboratorySlogan:
+      'You build scientific \r breakthroughs.\rWe make them\rstand out.',
+    laboratory: [
+      {
+        title: 'About Us',
+        type: 'paragraphs',
+        paragraphs: [
+          'The lab is based at Center for Molecular and Cellular Biology of Skolkovo Institute of Science and Technology. Our main research areas are protein bioinformatics, physics, and evolution.',
+          'One research direction of the lab is to improve prediction of protein stability change upon single mutation by combining machine learning and protein physics. Related topics are computational construction of more stable proteins and prediction of protein folding kinetics from protein three-dimensional structure. Another research direction is theoretical investigation of evolution with focus on multi-dimensional epistasis. More specifically, the lab develops computational methods and algorithms to find epistasis in experimental data and to estimate abundance of uni- and multi-dimensional epistasis in protein evolution.',
+        ],
+      },
+      {
+        title: 'Research',
+        type: 'blocks',
+        blocks: [
+          {
+            title: 'Protein Design',
+            description:
+              'Prediction of protein stability change upon single-point mutation by combining machine learning and protein physics. Computational construction of more stable proteins and prediction of protein folding kinetics from protein three-dimensional structure.',
+            image: '',
+          },
+          {
+            title: 'Epistasis',
+            description:
+              'Theoretical investigation of evolution with focus on multi-dimensional epistasis. Development of computational methods and algorithms to find epistasis in experimental data and to estimate abundance of uni- and multi-dimensional epistasis in protein evolution.',
+            image: '',
+          },
+        ],
+      },
+      {
+        title: 'Members',
+        type: 'cards',
+        cards: [
+          { title: 'Dmitriy Ivankov', subtitle: 'Superviser', image: '' },
+          {
+            title: 'Ekaterina Alaniia',
+            subtitle: 'Research Intern',
+            image: '',
+          },
+          { title: 'Natalia Sivitskaia', subtitle: 'PhD–1', image: '' },
+          { title: 'Marina Pak', subtitle: 'PhD–2', image: '' },
+          { title: 'Egor Bulavko', subtitle: 'MSc–1', image: '' },
+          {
+            title: 'Maria Carolina \r Erazo Muñoz',
+            subtitle: 'MSc–2',
+            image: '',
+          },
+          { title: 'Evgenii Zorin', subtitle: 'MSc–2', image: '' },
+        ],
+      },
+      {
+        title: 'Publications',
+        type: 'publications',
+        publications: [
+          {
+            title:
+              'Pak, M. A., & Ivankov, D. N. (2022). Best templates outperform homology models in predicting the impact of mutations on protein stability. Bioinformatics, 38(18), 4312–4320',
+            description:
+              'Prediction of protein stability change upon mutation (ΔΔG) is crucial for facilitating protein engineering and understanding of protein folding principles. Robust prediction of protein folding free energy change requires the knowledge of protein three-dimensional (3D) structure. In case, protein 3D structure is not available, one can predict the structure from protein sequence; however, the perspectives of ΔΔG predictions for predicted protein structures are unknown. The accuracy of using 3D structures of the best templates for the ΔΔG prediction is also unclear.\rTo investigate these questions, we used a representative set of seven diverse and accurate publicly available tools (FoldX, Eris, Rosetta, DDGun, ACDC-NN, ThermoNet and DynaMut) for stability change prediction combined with AlphaFold or I-Tasser for protein 3D structure prediction. We found that best templates perform consistently better than (or similar to) homology models for all ΔΔG predictors. Our findings imply using the best template structure for the prediction of protein stability change upon mutation if the protein 3D structure is not available.',
+            url: 'https://doi.org/10.1093/bioinformatics/btac515',
+          },
+          {
+            title:
+              'Zorin, E. M., Erazo, C. M., & Ivankov, D. N. (2022). Composite mutations give an extra insight into epistasis.',
+            description: '',
+            url: '',
+          },
+          {
+            title:
+              'Finkelstein, A. V., Bogatyreva, N. S., Ivankov, D. N., & Garbuzynskiy, S. O. (2022). Protein folding problem: enigma, paradox, solution. Biophysical Reviews.',
+            description: '',
+            url: '',
+          },
+          {
+            title:
+              'Pak, M. A., Markhieva, K. A., Novikova, M. S., Petrov, D. S., Vorobyev, I. S., Maksimova, E. S., Kondrashov, F. A., & Ivankov, D. N. (2021). Using AlphaFold to predict the impact of single mutations on protein stability and function. ',
+            description: '',
+            url: '',
+          },
+          {
+            title:
+              'Ivankov, D. N., & Finkelstein, A. V. (2020). Solution of Levinthal’s Paradox and a Physical Theory of Protein Folding Times. Biomolecules, 10(2), 250.',
+            description: '',
+            url: '',
+          },
+          {
+            title:
+              'Esteban, L. A., Lonishin, L. R., Bobrovskiy, D., Leleytner, G., Bogatyreva, N. S., Kondrashov, F. A., & Ivankov, D. N. (2019). HypercubeME: two hundred million combinatorially complete datasets from a single experiment. Bioinformatics. ',
+            description: '',
+            url: '',
+          },
+          {
+            title:
+              'Pokusaeva, V. O., Usmanova, D. R., Putintseva, E. V., Espinar, L., Sarkisyan, K. S., Mishin, A. S., Bogatyreva, N. S., Ivankov, D. N., Akopyan, A. V., Avvakumov, S. Ya., Povolotskaya, I. S., Filion, G. J., Carey, L. B., & Kondrashov, F. A. (2019). An experimental assay of the interactions of amino acids from orthologous sequences shaping a complex fitness landscape. PLOS Genetics, 15(4), e1008079.',
+            description: '',
+            url: '',
+          },
+        ],
+      },
+      { title: 'Thesis Defenses', type: 'annual' },
+      {
+        title: 'Applications',
+        type: 'blocks',
+        blocks: [
+          {
+            title: 'Sequence number',
+            image: '',
+          },
+          {
+            title: 'Fitland',
+            image: '',
+          },
+          {
+            title: 'ΔΔG datasets and predictors',
+            image: '',
+          },
+        ],
+      },
+      { title: 'Teaching', type: 'blocks' },
+    ],
     mutations_headers: [
       {
         text: 'protein name',
--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,7 +1,66 @@
 <template lang="pug">
-v-layout.justify-center.align-center(column style='min-height: 90vh')
-  h1.font-accent Ivankovlab
-  router-link(to='/datasets') Datasets project
+v-row.pt-12(style='background-color: #f7f7f7')
+  v-flex(xs1)
+  v-flex.text-left.ml-sm-8(xs10)
+    #first(style='height: 78vh')
+      h1#laboratory-name.sf {{ $t('laboratoryName') }}
+      #laboratory-slogan
+        h2.sf.text-md-h2.text-sm-h3(v-html='$t("laboratorySlogan")')
+        v-btn#contact.no-scale.mt-11(text, x-large, dark, depressed)
+          span.sf.font-weight-regular Contact us
+          v-icon.pl-2 mdi-arrow-right-circle
+    .pt-11.mt-11.pb-11.mb-11(
+      v-for='(section, s_id) in $t("laboratory")',
+      :key='s_id'
+    )
+      h2.sf.section-title.pb-10 {{ section.title }}
+      div(v-if='section.type == "paragraphs"') 
+        p.paragraph(v-for='paragraph in section.paragraphs') {{ paragraph }}
+      div(v-if='section.type == "blocks"') 
+        v-row(dense)
+          v-col.pr-10.justify-space-between(
+            v-for='(block, b_id) in section.blocks',
+            :key='b_id'
+          )
+            v-card.block(flat)
+              v-img.white--text.align-end(height='200px')
+              v-card-title.block-title {{ block.title }}
+              v-card-text.block-description {{ block.description }}
+      #cards.d-flex(v-if='section.type == "cards"')
+        v-card.ma-4.ml-0(
+          flat,
+          elevation=0,
+          v-for='(card, c_id) in section.cards',
+          :key='c_id'
+        )
+          v-img.white--text.align-end(height='150px')
+          v-card-title.ml-2.sf {{ card.title }}
+          v-card-subtitle.ml-2.mb-2.sf {{ card.subtitle }}
+      div(v-if='section.type == "publications"')
+        .publication.mb-8(
+          v-for='(publication, p_id) in section.publications',
+          :key='p_id'
+        ) 
+          p.publication-title.mb-1.sf {{ publication.title }}
+          input(
+            v-model='isPublicationOpen[p_id]',
+            type='checkbox',
+            :id='"p-" + p_id',
+            style='display: none'
+          )
+          label(:for='"p-" + p_id') 
+            a.sf 
+              span See More
+              v-icon(v-if='!isPublicationOpen[p_id]', color='#95CEC9') mdi-menu-right
+              v-icon(v-else, color='#95CEC9') mdi-menu-down
+          p.mt-8.mb-2.publication-description.sf(v-if='isPublicationOpen[p_id]') {{ publication.description }}
+          a.sf(v-if='isPublicationOpen[p_id]' :href='publication.url' target="_blank") {{ publication.url }}
+        span(v-if='isPublicationOpen[0]') {{ isPublicationOpen }}
+    div 
+      h2.section-title Contacts
+      span.text-h5.sf E-Mail: d.ivankov@skoltech.ru
+    //- router-link(to='/datasets') Datasets project
+  v-flex(xs1)
 </template>
 
 <script lang="ts">
@@ -9,5 +68,245 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 
 @Component
-export default class Root extends Vue {}
+export default class Root extends Vue {
+  isPublicationOpen: boolean[] = new Array<boolean>(
+    // @ts-ignore
+    this.$t('laboratory').filter(
+      (e) => e.type == 'publications'
+    )[0].publications.length
+  ).fill(false)
+}
 </script>
+<style scoped>
+#laboratory-name {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 127%;
+  /* identical to box height, or 51px */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: -0.02em;
+
+  color: #3b3d3c;
+}
+
+#laboratory-slogan {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-content: center;
+  align-self: center;
+  /* align-items: center; */
+  height: 70vh;
+}
+
+#laboratory-slogan > h2 {
+  white-space: pre-line;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 115%;
+  /* or 74px */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.02em;
+
+  color: #3b3d3c;
+
+  text-shadow: -2px 2px 8px rgba(0, 0, 0, 0.25);
+}
+
+#contact {
+  padding: 12px 24px;
+  gap: 8px;
+
+  width: 300px;
+  height: 56px;
+
+  /* theme/primary */
+  background: #95cec9;
+  box-shadow: -2px 2px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+}
+
+#contact > .v-btn__content > span {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px !important;
+  line-height: 32px;
+}
+
+.section-title {
+  font-style: normal;
+  font-weight: 800;
+  font-size: 4rem;
+  line-height: 127%;
+  /* or 81px */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.02em;
+
+  /* theme/text */
+  color: #3b3d3c;
+  text-shadow: -2px 2px 8px rgba(0, 0, 0, 0.25);
+}
+
+.paragraph {
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 32px;
+  max-width: 875px;
+  /* or 160% */
+
+  display: flex;
+  align-items: center;
+
+  /* theme/text */
+  color: #3b3d3c;
+
+  filter: drop-shadow(-2px 2px 8px rgba(0, 0, 0, 0.25));
+}
+
+.block {
+  height: 560px;
+  background: linear-gradient(
+      180deg,
+      rgba(59, 61, 60, 0) 46.88%,
+      rgba(59, 61, 60, 0.25) 92.19%
+    ),
+    #ffffff;
+  box-shadow: -2px 2px 12px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+}
+
+.block:hover {
+  transition: all ease-in-out 0.4s;
+  background: linear-gradient(
+      180deg,
+      rgba(149, 203, 206, 0) 46.88%,
+      rgba(149, 203, 206, 0.25) 92.19%
+    ),
+    #ffffff;
+}
+
+.block-title {
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 127%;
+  /* identical to box height, or 41px */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.02em;
+
+  /* theme/text */
+  color: #3b3d3c;
+
+  word-break: break-word !important;
+}
+
+.block-description {
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 127%;
+  /* or 25px */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.02em;
+
+  /* theme/text */
+  color: #3b3d3c;
+}
+
+#cards {
+  flex-wrap: wrap;
+}
+#cards > * {
+  aspect-ratio: 1;
+  filter: drop-shadow(-1px 1px 6px rgba(0, 0, 0, 0.15));
+}
+@media screen and (max-width: 900px) {
+  #cards > * {
+    flex: 0 0 33%;
+  }
+}
+@media screen and (min-width: 900px) {
+  #cards > * {
+    flex: 0 0 21%;
+  }
+}
+
+#cards > * > .v-card__title {
+  white-space: pre-line;
+  word-break: break-word;
+
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 127%;
+  /* or 20px */
+
+  letter-spacing: 0.01em;
+
+  /* theme/primary */
+  color: #95cec9;
+}
+
+#cards > * > .v-card__subtitle {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 127%;
+  /* or 15px */
+
+  /* theme/text */
+  color: #3b3d3c;
+}
+
+.v-image__image--preload {
+  background: #fff !important;
+}
+.publication > p.publication-title {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 32px;
+  /* or 160% */
+
+  /* theme/text */
+  color: #3b3d3c;
+}
+.publication > p.publication-description {
+  font-size: 18px;
+  line-height: 32px;
+  /* or 178% */
+
+  /* theme/text */
+  color: #3b3d3c;
+}
+
+.publication > label > a {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 32px;
+  /* or 160% */
+  
+  /* theme/primary */
+  color: #95cec9 !important;
+}
+.publication > a {
+  font-size: 16px;
+  text-decoration: none;
+  color: #95cec9 !important;
+}
+</style>
