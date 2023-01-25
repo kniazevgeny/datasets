ec6130f96b9c6e44b074573760db37f1142251d1
kniazevgeny <eugene.kniazev@gmail.com>
2023-01-25 18:52:05 +0300

    feat: implement overlaps feature

--- src/components/ComparisonCard.vue
+++ src/components/ComparisonCard.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 v-card(color='accent', id='comparison')
-  .d-flex.flex-column(v-if='items.length == 2')
+  .d-flex.flex-column(v-if='items.length == 2 && isDatasetsReady')
     v-simple-table(style='background-color: transparent !important')
       template(v-slot:default)
         thead
@@ -17,6 +17,28 @@ v-card(color='accent', id='comparison')
             td.text-left {{ row.param }}
             td {{ row.dataset1 }}
             td {{ row.dataset2 }}
+    v-divider.py-2 
+    .d-flex.flex-column.pl-4.text-left()
+      span.overlap Overlaps
+      span // Feature description
+      span.mt-3(v-if='isDatasetsReady')
+        span.font-weight-bold.blueComparison--text {{ dataset1.name }}&nbsp;
+        span overlaps with
+        span.font-weight-bold.pinkComparison--text &nbsp;{{ dataset2.name }}&nbsp;
+        span at
+        #pident.px-1
+          v-text-field(v-model='pident', dense, outlined, hide-details, @change='resetOverlaps')
+        span sequence identity
+      .d-flex.flex-column(v-if='showOverlapsResult')
+        span.mt-3
+          span(v-if='all_overlap') All data overlap at {{pident}} cutoff.
+          span(v-if='no_overlap') No overlap at {{pident}} cutoff.
+          span(v-if='!all_overlap && !no_overlap') Percent of overlapping data: {{percent_of_overlapping}}
+        span(v-if='overlappingProteinsLength') Overlapping proteins: {{ overlappingProteinsLength }}
+        a(v-if='showOverlapsBtn', :href='`${base}/download_overlap?_id=${overlap_id}`') Download dataset without overlaps
+        a(v-if='overlappingProteinsLength', :href='`${base}/download_overlapping_proteins?_id=${overlap_id}`') Download overlapping proteins
+      v-btn.mt-2.mb-6(v-else, small, dense, width='25ch', color='primary', @click='calculateOverlaps') Calculate overlaps
+
   .d-flex.pa-4(v-else)
     //- explainer
     span.text-left Select 2 datasets to compare them and calculate overlaps
@@ -30,6 +52,8 @@ import { namespace } from 'vuex-class'
 const AppStore = namespace('AppStore')
 
 import { Dataset } from '@/models/Dataset'
+
+import { base, calculateOverlap } from '@/utils/api'
 
 interface Selection {
   isSelected: boolean
@@ -51,8 +75,40 @@ export default class ComparisonCard extends Vue {
 
   comparisonRows: object[] = []
 
+  showOverlapsResult = false 
+  isDatasetsReady = false
+
+  overlap_id = ''
+
+  all_overlap = false
+  no_overlap = false
+  percent_of_overlapping = 0
+  overlappingProteinsLength = 0
+  showOverlapsBtn = false
+  pident = 25
+
+  get base() {
+    return base
+  }
+
+  resetOverlaps() {
+    this.showOverlapsResult = false
+  }
+
   swap() {
     this.$emit('swap')
+  }
+
+  async calculateOverlaps() {
+    if (typeof this.dataset1 == typeof undefined || typeof this.dataset2 == typeof undefined) return;
+    const response = (await calculateOverlap((this.dataset1 as Dataset).name, (this.dataset2 as Dataset).name, this.pident)).data
+    this.all_overlap = response.all_overlap
+    this.no_overlap = response.no_overlap
+    this.percent_of_overlapping = response.percent_of_overlapping
+    this.overlappingProteinsLength = response.overlapping_proteins.length
+    if (response.dataset1_data_no_overlap_hashes.length) this.showOverlapsBtn = true
+    this.showOverlapsResult = true
+    this.overlap_id = response._id
   }
 
   @Watch('items') 
@@ -61,6 +117,10 @@ export default class ComparisonCard extends Vue {
     // get data for each dataset from store
     this.dataset1 = this.datasets?.find(el => el._id == this.items[0]._id) as Dataset
     this.dataset2 = this.datasets?.find(el => el._id == this.items[1]._id) as Dataset
+
+    // request overlaps info
+    this.calculateOverlaps()
+    this.isDatasetsReady = true
 
     // generate elements to display comparison
     if (typeof this.dataset1 == typeof undefined || typeof this.dataset2 == typeof undefined) return;
@@ -111,5 +171,28 @@ export default class ComparisonCard extends Vue {
 }
 .comparison-row > td:nth-child(3) {
   color: var(--v-pinkComparison)
+}
+.overlap {
+  font-size: 20px;
+  line-height: 24px;
+}
+#comparison .v-data-table__wrapper {
+  overflow-x: hidden;
+}
+#pident {
+  display: inline-block;
+  width: 5ch;
+  font-size: 14px;
+}
+#pident .v-input__slot {
+  padding: 0 6px;
+  font-size: 14px;
+}
+#padding .v-text-field__slot {
+  font-weight: 500;
+}
+#pident input {
+  text-align: center;
+  padding: 2px 0 2px;
 }
 </style>
